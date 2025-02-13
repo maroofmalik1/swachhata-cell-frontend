b96bf90592568383afc3937a84f251150586b672
Mohd Maroof <146874434+maroofmalik1@users.noreply.github.com>
2025-02-12 21:44:34 -0800

    Update Success.jsx

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -8,7 +8,8 @@ function Success({ setAppState, AppStates }) {
 
   return (
     <div>
-      <p>Success</p>
+      <img src="./assets/iamges/__Iphone-spinner-1.gif" alt="Loading" width="50" height="50">
+      //<p>Success</p>
       <button onClick={formResetHandler}>Fill Form Again</button>
     </div>
   );
